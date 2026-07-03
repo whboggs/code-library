66c6ab4aeb2f84add128ef6c6e41265bd1f1de6a
Boggs <41301253+whboggs@users.noreply.github.com>
2026-07-02 23:44:42 -0600

    Update lead-config.js with additional comments
    Added comments for clarity on required parameters.

--- meta/pixel/lead/lead-config.js
+++ meta/pixel/lead/lead-config.js
@@ -1,4 +1,6 @@
 <script>
+  // Note: Requires cJS - Traffic Source and cJS - Ad Placement.
+  // Delete these lines if you do not want to add these parameters.
 fbq('track', 'Lead', {
   lead_type: 'Form Submit', // Define Lead Type
   traffic_source: '{{cJS - Traffic Source}}', // Finds Last Touch traffic source
